0bdc30fc56ab016f50a30b1fd734bfc1a666d0e0
LJebry <Jerryrobayo1130@gmail.com>
2026-05-04 23:40:24 -0400

    Lower MVP file size limit

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,6 +1,6 @@
 export const FILE_CHUNK_SIZE = 64 * 1024;
 
-export const MAX_FILE_SIZE_BYTES = 1024 * 1024 * 1024;
+export const MAX_FILE_SIZE_BYTES = 20 * 1024 * 1024;
 
 export const WEBRTC_ICE_SERVERS = [
   { urls: "stun:stun.l.google.com:19302" }
